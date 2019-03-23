392e3a02e3dbdfbb5f6ead0a2440018e781cbf57
fellyph <fellyph.cintra@gmail.com>
2019-03-23 16:00:59 +0000

    adding events to install pwa

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,28 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+let myPrompt;
+const pwaAlert = document.querySelector('.pwa__alert');
+const btnPWA = document.querySelector('.pwa__alert__btn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  console.log('pronto para instalar', e);
+  myPrompt = e;
+
+  pwaAlert.style.display = 'block';
+});
+
+btnPWA.addEventListener('click', () => {
+  pwaAlert.style.display = 'none';
+  myPrompt.prompt();
+  myPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult === 'accepted') {
+        console.log('Usuário aceitou o prompt');
+      } else {
+        console.log('Usuário cancelou o prompt');
+      }
+    });
+});
